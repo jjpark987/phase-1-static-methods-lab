8cb9b2bd0904ecec2e3785a65ff9d6b6df7cfe1d
Jason Park <jjpark987@gmail.com>
2022-12-30 16:13:36 -0800

    commit tree 06a8be285ae6cf68efd97434203aae183c2af464

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+
+  static titleize(string) {
+    const words = string.split(' ')
+    words[0] = this.capitalize(words[0])
+    for(let i = 0; i < words.length; i++) {
+      switch(words[i]) {
+        case 'the':
+          break
+        case 'a':
+          break
+        case 'an':
+          break
+        case 'but':
+          break
+        case 'of':
+          break
+        case 'and':
+          break
+        case 'for':
+          break
+        case 'at':
+          break
+        case 'by':
+          break
+        case 'from':
+          break
+        default:
+          words[i] = this.capitalize(words[i])
+      }
+    }
+    return words.join(' ')
+  }
 }
